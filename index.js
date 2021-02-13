c3e599b0afdbaf439576dcd5e899f095c376270a
Dat Tran <dattran@Dats-MacBook-Pro.local>
2021-02-13 09:32:15 +0700

    2nd

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 let luckyNumberContent = document.getElementById('lucky-number');
+let luckyBtn = document.getElementById('lucky-number-btn');
+let luckySuccess = document.getElementById('lucky-number-success');
 let randomNumber = null;
+luckySuccess.style.display = 'none';
 
 document.getElementById('lucky-number-btn').addEventListener( 'click',() => {
     randomNumber = Math.floor((Math.random() * 100) + 1);
     luckyNumberContent.innerHTML = randomNumber;
+    luckySuccess.style.display = 'block';
+    luckyBtn.style.display = 'none';
 });
 
